6872733841e75967b9bdef83ee2d1798f8032d1b
Jason Kaufmann <41923667+jasonkaufmann@users.noreply.github.com>
2021-07-11 00:13:49 -0700

    new changes

--- DLS/DLS/MainWindow.xaml.cs
+++ DLS/DLS/MainWindow.xaml.cs
@@ -169,7 +169,9 @@ namespace DigitalLogicSimulatorUpdater
             try
             {
                 string onlineVersion = ((Version)e.UserState).ToString();
-                ZipFile.ExtractToDirectory(gameZip, rootPath, true);
+                FileStream zipToOpen = new FileStream(gameZip, FileMode.Open);
+                ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update);
+                ZipArchiveExtensions.ExtractToDirectory(archive , rootPath, true);
                 File.Delete(gameZip);
                 File.WriteAllText(versionFile, onlineVersion);
 
@@ -201,6 +203,38 @@ namespace DigitalLogicSimulatorUpdater
             else if (Status == LauncherStatus.failed)
             {
                 CheckForUpdates();
+            }
+        }
+    }
+
+    public static class ZipArchiveExtensions
+    {
+        public static void ExtractToDirectory(this ZipArchive archive, string destinationDirectoryName, bool overwrite)
+        {
+            if (!overwrite)
+            {
+                archive.ExtractToDirectory(destinationDirectoryName);
+                return;
+            }
+
+            DirectoryInfo di = Directory.CreateDirectory(destinationDirectoryName);
+            string destinationDirectoryFullPath = di.FullName;
+
+            foreach (ZipArchiveEntry file in archive.Entries)
+            {
+                string completeFileName = Path.GetFullPath(Path.Combine(destinationDirectoryFullPath, file.FullName));
+
+                if (!completeFileName.StartsWith(destinationDirectoryFullPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new IOException("Trying to extract file outside of destination directory. See this link for more info: https://snyk.io/research/zip-slip-vulnerability");
+                }
+
+                if (file.Name == "")
+                {// Assuming Empty for Directory
+                    Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                    continue;
+                }
+                file.ExtractToFile(completeFileName, true);
             }
         }
     }
